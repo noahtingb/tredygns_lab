140fc7e187b9d9b6295a6cf955f99b5017c2555d
Lazergurka <Herrj.gyllenstedt@live.se>
2025-05-28 22:05:54 +0200

    Implementerat NIST funktioner I huvud branch
    Så som titeln säger

---
+++ linje_funktioner.py
@@ -0,0 +1,37 @@
+#kod för inläsning av tabelerad data för spektral linjer
+import pandas as pd
+import numpy as np
+
+def get_lines(element, raw = False):
+    filnamn = ""
+
+    match element:
+        case "Cd":
+            filnamn = "excels\Cd_I_lines.csv"
+
+        case "Na":
+            filnamn = "excels\\Na_I_lines.csv"
+
+        case "H":
+            filnamn = "excels\H_I_lines.csv"
+
+        case "Ne":
+            filnamn = "excels\\Ne_I_lines.csv"
+
+        case _:
+            raise Exception("Inkorrekt elementförkortning eller ej med i tabulerade element.")
+    
+    retur_varde = pd.read_csv(filnamn)
+    if(not raw):
+        retur_varde = retur_varde.replace(['[\[\]="*wrbl()f]'],[""], regex = True)
+
+    return retur_varde
+
+def NIST_spektrum(element, max_peak):
+    linjer = get_lines(element)
+    intensiteter = pd.to_numeric(linjer["intens"])
+    wl = pd.to_numeric(linjer["obs_wl_air(nm)"])
+    max_intens = np.max(intensiteter)
+    norm_intens = intensiteter/max_intens
+    
+    return wl, norm_intens*max_peak
