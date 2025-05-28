fd525ec9087bbbcd5f15737129559f60a1608899
Lazergurka <Herrj.gyllenstedt@live.se>
2025-05-28 17:39:46 +0200

    Teoretiska spektral linjer och funktion för att hämta implementerat
    Lagt till observerade spektral linjer för neutrala atomer från NIST databasen. Har även provat att jobba med git.
    Ytterligare har funktion för att hämta datan lagts till

---
+++ linje_funktioner.py
@@ -0,0 +1,22 @@
+#kod för inläsning av tabelerad data för spektral linjer
+import pandas as pd
+def get_lines(element):
+    filnamn = ""
+
+    match element:
+        case "Cd":
+            filnamn = "excels\Cd_I_lines.csv"
+
+        case "Na":
+            filnamn = "excels\Na_I_lines.csv"
+
+        case "H":
+            filnamn = "excels\H_I_lines.csv"
+
+        case "Ne":
+            filnamn = "excels\Ne_I_lines.csv"
+
+        case _:
+            raise Exception("Inkorrekt elementförkortning eller ej med i tabulerade element.")
+
+    return pd.read_csv(filnamn)
