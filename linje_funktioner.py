42e5577298ea7d2deb80ef7454d4bcaf585161bb
Lazergurka <Herrj.gyllenstedt@live.se>
2025-05-28 21:08:01 +0200

    NIST funktioner
    Lagt till funktioner för att lättare hantera data från NIST databasen

--- linje_funktioner.py
+++ linje_funktioner.py
@@ -1,6 +1,8 @@
 #kod för inläsning av tabelerad data för spektral linjer
 import pandas as pd
-def get_lines(element):
+import numpy as np
+
+def get_lines(element, raw = False):
     filnamn = ""
 
     match element:
@@ -18,5 +20,18 @@ def get_lines(element):
 
         case _:
             raise Exception("Inkorrekt elementförkortning eller ej med i tabulerade element.")
+    
+    retur_varde = pd.read_csv(filnamn)
+    if(not raw):
+        retur_varde = retur_varde.replace(['[\[\]="*wrbl()]'],[""], regex = True)
 
-    return pd.read_csv(filnamn)
+    return retur_varde
+
+def NIST_spektrum(element, max_peak):
+    linjer = get_lines(element)
+    intensiteter = pd.to_numeric(linjer["intens"])
+    wl = pd.to_numeric(linjer["obs_wl_air(nm)"])
+    max_intens = np.max(intensiteter)
+    norm_intens = intensiteter/max_intens
+    
+    return wl, norm_intens*max_peak
